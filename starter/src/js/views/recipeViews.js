632c554f00d88722377a5eccb838ac7144484cf1
Zidane Innis <zidaneinnis@gmail.com>
2022-06-23 22:41:50 -0500

    Update

--- starter/src/js/views/recipeViews.js
+++ starter/src/js/views/recipeViews.js
@@ -3,7 +3,7 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-import { numberToFraction } from '../helpers.js';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -124,7 +124,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? numberToFraction(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
